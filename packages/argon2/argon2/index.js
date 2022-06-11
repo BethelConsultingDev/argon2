8d8aff0dc4856d86952b50332d70b6195a62dfbb
BethelConsultingDev <81204440+BethelConsultingDev@users.noreply.github.com>
2022-06-10 21:31:43 -0400

    Update index.js

--- packages/argon2/argon2/index.js
+++ packages/argon2/argon2/index.js
@@ -1,10 +1,29 @@
+const argon2 = require("argon2");
+const crypto = require("crypto");
+
 function main(args) {
+
+  crypto.randomFill(new Uint8Array(16), async (err, salt) => {
+    if (err) throw err;
+
+    //Generate encryption Key using the secret.
+    const key = await argon2.hash(SecuritySettings.SecretKey, {
+        type: argon2.argon2id,
+        memoryCost: 32*1024, //32MB in kb
+        hashLength: 32,
+        parallelism: 2,
+        timeCost: 3,
+        raw: true,
+        salt: salt
+    });
+
     let name = args.name || 'stranger'
     let greeting = 'Hello ' + name + '!'
     console.log(greeting)
-    return {"body": greeting}
+    return {"body": key}
 
+  });
     
-  }
+}
 
 exports.main = main
